c899767afa3f15ec0d6aaf93326c1c392d30e560
Nikita Lee <mrmitik.2003@gmail.com>
2023-10-20 21:05:29 +0300

    some changes

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ chel = Carpenter()
 
 chel.set_name(input('Введите имя '))
 chel.set_employee_number(input('Введите номер сотрудника '))
+chel.set_qualification(input('Введите уровень квалификации '))
 chel.set_shift_number(input('Введите номер смены '))
 chel.set_hourly_wage(input('Введите почасовую оплату '))
 
 print('Имя: ', chel.get_name())
 print('Номер сотрудника: ', chel.get_employee_number())
+print('Квалификация: ', chel.get_qualification())
 print(chel.get_shift_number())
 print('Ставка: ', chel.get_hourly_wage())
