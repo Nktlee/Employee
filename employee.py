a6610f62007a80a7f2cd775966b64e642666340c
Nikita Lee <mrmitik.2003@gmail.com>
2023-10-20 18:29:43 +0300

    some changes and add product

--- employee.py
+++ employee.py
@@ -1,9 +1,10 @@
 from human import Human
 
 class Employee(Human):
-    def __init__(self, name = 'name', surname = 'surname', address = 'address', phone_number = 1, employee_number = 123):
+    def __init__(self, name = 'name', surname = 'surname', address = 'address', phone_number = 1, employee_number = 123, qualification = 'worker'):
         super().__init__(name, surname, address, phone_number)
         self.__employee_number = employee_number
+        self.__qualification = qualification
 
     def get_employee_number(self):
         return self.__employee_number
@@ -15,3 +16,22 @@ class Employee(Human):
         except ValueError:
             print('Ошибка: введите целое число')
             self.set_employee_number(input('Введите номер сотрудника '))
+    
+    def get_qualification(self):
+        return self.__qualification
+    
+    def set_qualification(self, qualification):
+        try:
+            qualification = int(qualification)
+            if qualification == 1:
+                self.__qualification = "новичок"
+            elif qualification == 2:
+                self.__qualification = "средний работник"
+            elif qualification == 3:
+                self.__qualification = "профессионал"
+            else:
+                print('Ошибка: введите уровень квалификации (1 - новичек, 2 - средний работник, 3 - профессионал)')
+                self.set_qualification(input('Введите уровень квалификации '))
+        except ValueError:
+            print('Ошибка: введите целое число')
+            self.set_qualification(input('Введите уровень квалификации '))
---
+++ product.py
@@ -0,0 +1,78 @@
+class Product:
+    def __init__(self, name = 'name', width = 1, length = 1, height = 1, weight = 1, coefficient = 1):
+        self.__name = name
+        self.__width = width
+        self.__length = length
+        self.__height = height
+        self.__weight = weight
+        self.__coefficient = coefficient
+
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, name):
+        self.__name = name
+    
+    def get_width(self):
+        return self.__width
+    
+    def set_width(self, width):
+        try:
+            int(width)
+            self.__width = width
+        except ValueError:
+            print('Ошибка: введите целое число ')
+            self.set_width(input('Введите ширину '))
+
+    def get_length(self):
+        return self.__length
+    
+    def set_length(self, length):
+        try:
+            int(length)
+            self.__length = length
+        except ValueError:
+            print('Ошибка: введите целое число')
+            self.set_length(input('Введите длину '))
+
+    def get_height(self):
+        return self.__height
+    
+    def set_height(self, height):
+        try:
+            int(height)
+            self.__height = height
+        except ValueError:
+            print('Ошибка: введите целое число')
+            self.set_height(input('Введите высоту '))
+
+    def get_weight(self):
+        return self.__weight
+    
+    def set_weight(self, weight):
+        try:
+            int(weight)
+            self.__weight = weight
+        except ValueError:
+            print('Ошибка: введите целое число')
+            self.set_weight(input('Введите вес '))
+
+    def get_coefficient(self):
+        return self.__coefficient
+    
+    def set_coefficient(self, coefficient):
+        try:
+            coefficient = int(coefficient)
+            if coefficient == 1:
+                self.__coefficient = "легко"
+            elif coefficient == 2:
+                self.__coefficient = "средне"
+            elif coefficient == 3:
+                self.__coefficient = "сложно"
+            else:
+                print('Ошибка: введите сложность производства (1 - легко, 2 - средне, 3 - сложно)')
+                self.set_coefficient(input('Введите сложность производства '))
+        except ValueError:
+            print('Ошибка: введите целое число')
+            self.set_coefficient(input('Введите сложность производства '))
+        
